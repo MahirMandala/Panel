97649fd7ffc92ec85eab3d46d0ac64a6932d7dd7
Mahir Mandala <68174211+MahirMandala@users.noreply.github.com>
2023-11-26 11:38:16 +0700

    Update and rename Penghancur.sh to LoL.sh

--- LoL.sh
+++ LoL.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {1..9}; do
+for i in {1..5}; do
     screen -dmS bw$i bash -c "
         counter=0
         download_path=/root/download/$i
@@ -10,7 +10,7 @@ for i in {1..9}; do
             echo \"Running the script for the \$counter time(s) in session bw$i\"
 
             rm -rf \$download_path/*
-            wget -P \$download_path https://ash-speed.hetzner.com/10GB.bin
+            wget -P \$download_path https://bin.kartolo.cloud/100MB.bin
             rm -rf \$download_path/*
             clear
             
